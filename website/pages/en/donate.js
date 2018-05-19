cbd507a887d97c6d2a607ab70a49510f2bb7f46b
Alex Anderson <alexanderson1993@gmail.com>
2018-05-19 07:30:23 -0600

    Fix the donate page and add an update

--- website/pages/en/donate.js
+++ website/pages/en/donate.js
@@ -16,12 +16,18 @@ const DonationForm = () => (
       <a href="https://github.com/thorium-sim/thorium" className="button">
         Get the Code
       </a>
-      <h2>Donation</h2>
+      <h2>Purchase & Donation</h2>
       <p>
         Thorium is being developed by a small number of contributors in their
-        spare time. Your donations help to create new and exciting features and
-        content which you can use in your space center. As a Thorium Donor, you
-        recieve the following benefits:
+        spare time. To fund Thorium development, you can purchase the bundled
+        version of Thorium for $5.00. This includes everything you need to get
+        started with your own Space Edventure.
+      </p>
+      <p>
+        Additionally, you can donate any amount to Thorium. Your donations help
+        to create new and exciting features and content which you can use in
+        your space center. As a Thorium Donor, you recieve the following
+        benefits:
       </p>
       <ul>
         <li>
@@ -34,16 +40,13 @@ const DonationForm = () => (
           A special Kiosk program for Thorium, locking the computer to only
           display Thorium.
         </li>
+        <li>Your company's logo on the front page of this website.</li>
+
+        <li>Increased priority for feature requests.</li>
         <li>
           A warm feeling knowing that you have contributed to creating excellent
           starship simulator software.
         </li>
-      </ul>
-      <p>Education and Commercial donors get additional benefits:</p>
-      <ul>
-        <li>Your company's logo on the front page of this website.</li>
-
-        <li>Increased priority for feature requests.</li>
       </ul>
       <p>Your donation will go toward several great causes: </p>
       <ul>
@@ -65,22 +68,21 @@ const DonationForm = () => (
       <p>
         If you have any problems with your donation, need to cancel your
         subscription or want to request a refund, please{" "}
-        <a href="mailto:contact@thoriumsim.com">contact us</a>.
+        <a href="mailto:alex@fyreworks.us">contact us</a>.
       </p>
     </main>
     <aside className="sidebar">
       <h1>Donate</h1>
-      <em>
+      {/* <em>
         We are currently not accepting donations while we upgrade our backend
         system. In the meantime, if you are interested in donating in the future
         and would like the built version of Thorium, or if you would like to
         support Thorium's development in some other way, please reach out:{" "}
         <a href="mailto:alex@fyreworks.us">alex@fyreworks.us</a>
-      </em>
+      </em> */}
       <div id="donation" />
     </aside>
-    {/* <script src="/scripts/donation.js">
-      </script> */}
+    <script src="/scripts/donation.js" />
   </Container>
 );
 
